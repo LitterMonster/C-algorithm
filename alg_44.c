a1fecb36760e7201fb74d48bedafd310122d7397
zhangtao <sspu000000@163.com>
2016-03-04 16:05:29 +0800

    Modify alg_44.c

--- alg_44.c
+++ alg_44.c
@@ -1,43 +1,22 @@
 #include<stdio.h>
-#include<string.h>
-#include<math.h>
+#include"string.h"
 int main(void)
 {
-	char str1[10], str2[10];
-	char *p, *q;
+	char str1[20], str2[20];
 	scanf("%s", str1);
 	scanf("%s", str2);
-	p = str1;
-	q = str2;
-	int flag1, flag2, flag3;
-	flag1 = flag2 = flag3 = 0;
-	if (strlen(str1) != strlen(str2))
-		printf("1");
-	else
-	{
-		if (flag1 == 0)
-		while(*p != '\0' && *q != '\0')
-		{
-			if (*(p++) != *(q++))
-				if (*p - *q != 32 || *q -*p != -32)
-					flag1 = 1;
-		}
-		if (flag1 == 1)
-			printf("4");
-		else 
-			printf("2");
-		if (flag3 == 0)	
-		while(*p != '\0' && *q != '\0')
-		{
-			if (*p - *q == 32 || *q - *p == -32) 
-			{
-				flag3 = 0;
-			}
-			else
-				flag3 == 1;
-		}
-	}
+	int len1 = strlen(str1);
+	int len2 = strlen(str2);
 	
-	if (flag3 == 1)
-		printf("3");
+	if (len1 != len2)
+		printf("1\n");
+	else 
+		if (strcmp(str1, str2) == 0)
+			printf("2\n");
+		else if (strcmpi(str1, str2) == 0)
+			printf("3\n");
+		else
+			printf("4\n");
+	
+	
 }
